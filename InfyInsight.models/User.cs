1ca2ce5401593f258c174c9f5d2cd708ae45752b
gopkumr <gopakumar.r@live.com>
2015-07-26 11:37:56 +0530

    Added store class

--- InfyInsight.models/User.cs
+++ InfyInsight.models/User.cs
@@ -12,5 +12,6 @@ namespace InfyInsight.models
         public string Email { get; set; }
         public Address Address { get; set; }
         public string PhoneNumber { get; set; }
+
     }
 }
--- InfyInsight.store/IStoreRepository.cs
+++ InfyInsight.store/IStoreRepository.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InfyInsight.models;
 
 namespace InfyInsight.store
 {
     public interface IStoreRepository
     {
+        Guid AddProduct(Product product);
+        Product AddInventory(Guid productId, int quantity );
+        Product GetProduct(Guid id);
+        Product GetProduct(Product product);
+        IEnumerable<Product> SearchProduct(string WildCardString);
+
+        User LoginUser(string userName, string password);
+        User RegisterUser(User user);
+
+        Order AddProductToCart(Guid productId, int quantity);
+        Order RemoveProductToCart(Guid productId, int quantity);
+        bool CheckoutCart(Guid orderId);
     }
 }
