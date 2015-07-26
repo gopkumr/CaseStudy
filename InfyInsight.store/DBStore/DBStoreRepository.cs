84527bfe6a38464c1f15de7a30b585ec60de3b95
gopkumr <gopakumar.r@live.com>
2015-07-26 14:54:57 +0530

    Added DB Infrasturcture & unit tests

--- InfyInsight.store/DBStore/DBStoreRepository.cs
+++ InfyInsight.store/DBStore/DBStoreRepository.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace InfyInsight.store.DBStore
 {
     public class DBStoreRepository:IStoreRepository
     {
+        private DBStoreEntities _dbContext;
+        public DBStoreRepository()
+        {
+            _dbContext = new DBStoreEntities();
+        }
+
+        public DBStoreRepository(DBStoreEntities dbEntities)
+        {
+            _dbContext = dbEntities;
+        }
+
         public Guid AddProduct(models.Product product)
         {
-            throw new NotImplementedException();
+            var id = Guid.NewGuid();
+            product.ProductId = id;
+            var data = this.SerializeJson<models.Product>(product);
+            _dbContext.Products.Add(new Product
+            {
+                Id = id,
+                Product1 = data
+            });
+            _dbContext.SaveChanges();
+            return id;
         }
 
         public models.Product AddInventory(Guid productId, int quantity)
@@ -56,6 +77,18 @@ namespace InfyInsight.store.DBStore
         public bool CheckoutCart(Guid orderId)
         {
             throw new NotImplementedException();
+        }
+
+        private string SerializeJson<T>(T source)
+        {
+            var seralizedData = JsonConvert.SerializeObject(source);
+            return seralizedData;
+        }
+
+        private T DeSerializeJson<T>(string source)
+        {
+            var deseralizedData = JsonConvert.DeserializeObject<T>(source);
+            return deseralizedData;
         }
     }
 }
