828590c14bea36bdb1eb1aef7f8932b7cdd831b2
gopkumr <gopakumar.r@live.com>
2015-07-26 14:56:18 +0530

    Added Nuget Packages and Test Files

---
+++ InfyInsight.test/DBStoreTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using InfyInsight.store.DBStore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+
+namespace InfyInsight.test
+{
+    [TestClass]
+    public class DBStoreTest
+    {
+        private DBStoreEntities _dbEntities;
+        private DbSet<Product> _dbProducts;
+
+        public DBStoreTest()
+        {
+            _dbEntities = Rhino.Mocks.MockRepository.GenerateMock<DBStoreEntities>();
+            _dbProducts = Rhino.Mocks.MockRepository.GenerateMock<DbSet<Product>>();
+        }
+
+        [TestMethod]
+        public void AddProduct_Test()
+        {
+            //Arrange
+            _dbEntities.Stub(q => q.Products).Return(_dbProducts);
+            _dbEntities.Stub(q => q.SaveChanges).Return(1);
+            var dbStore = new DBStoreRepository(_dbEntities);
+
+            //Act
+            var id = dbStore.AddProduct(new models.Product()
+            {
+                LongDescription = "Test Long Desc",
+                Price = 20.50M,
+                ProductType = models.ProductType.Book
+            });
+
+            //Assert
+            Assert.IsTrue(id != Guid.Empty);
+        }
+    }
+}
