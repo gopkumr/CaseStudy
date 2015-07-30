dd478b87e451677e45ab16ac8cb92706b60fa95c
gopkumr <gopakumar.r@live.com>
2015-07-30 08:15:02 +0530

    Fixed template issues

--- InfyInsight.web/app/scripts/app.js
+++ InfyInsight.web/app/scripts/app.js
@@ -22,29 +22,29 @@ angular
       .state('base', {
         abstract: true,
         url: '',
-        templateUrl: 'views/base.html'
+        templateUrl: '/app/views/base.html'
       })
         .state('login', {
           url: '/login',
           parent: 'base',
-          templateUrl: 'views/login.html',
+          templateUrl: '/app/views/login.html',
           controller: 'LoginCtrl'
         })
         .state('dashboard', {
           url: '/dashboard',
           parent: 'base',
-          templateUrl: 'views/dashboard.html',
+          templateUrl: '/app/views/dashboard.html',
           controller: 'DashboardCtrl'
         })
           .state('overview', {
             url: '/overview',
             parent: 'dashboard',
-            templateUrl: 'views/dashboard/overview.html'
+            templateUrl: '/app/views/dashboard/overview.html'
           })
           .state('reports', {
             url: '/reports',
             parent: 'dashboard',
-            templateUrl: 'views/dashboard/reports.html'
+            templateUrl: '/app/views/dashboard/reports.html'
           });
 
   });
